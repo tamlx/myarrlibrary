ef5ff2ca6e5462545ef721b2b6d593de0b020f85
QTCTEK COMP <admin@QTCTEKs-iMac.local>
2018-09-07 14:51:52 +0700

    add database, typingtext

---
+++ myaarlibrary/src/main/java/b/laixuantam/myaarlibrary/database/orm/inflater/field/DefaultFieldInflater.java
@@ -0,0 +1,22 @@
+package b.laixuantam.myaarlibrary.database.orm.inflater.field;
+
+import android.database.Cursor;
+
+import b.laixuantam.myaarlibrary.database.orm.util.ReflectionUtil;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by Łukasz Wesołowski on 03.08.2016.
+ */
+public class DefaultFieldInflater extends FieldInflater {
+
+    public DefaultFieldInflater(Field field, Cursor cursor, Object object, Class<?> fieldType) {
+        super(field, cursor, object, fieldType);
+    }
+
+    @Override
+    public void inflate() {
+        ReflectionUtil.setFieldValueFromCursor(cursor, field, object);
+    }
+}
---
+++ myaarlibrary/src/main/java/b/laixuantam/myaarlibrary/database/orm/inflater/field/EntityFieldInflater.java
@@ -0,0 +1,30 @@
+package b.laixuantam.myaarlibrary.database.orm.inflater.field;
+
+import android.database.Cursor;
+import android.util.Log;
+
+import b.laixuantam.myaarlibrary.database.orm.SugarRecord;
+import b.laixuantam.myaarlibrary.database.orm.helper.NamingHelper;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by Łukasz Wesołowski on 03.08.2016.
+ */
+public class EntityFieldInflater extends FieldInflater {
+    private static final String LOG_TAG = "EntityFieldInflater";
+
+    public EntityFieldInflater(Field field, Cursor cursor, Object object, Class<?> fieldType) {
+        super(field, cursor, object, fieldType);
+    }
+
+    @Override
+    public void inflate() {
+        try {
+            long id = cursor.getLong(cursor.getColumnIndex(NamingHelper.toColumnName(field)));
+            field.set(object, (id > 0) ? SugarRecord.findById(fieldType, id) : null);
+        } catch (IllegalAccessException e) {
+            Log.e(LOG_TAG, String.format("Error while inflating entity field %s", field), e);
+        }
+    }
+}
